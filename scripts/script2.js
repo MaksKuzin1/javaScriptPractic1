7ed4fe9826f0cb583a626f4efed4fd981c5f6f7b
Maks Kuzin <maks.kuzin5544@gmail.com>
2025-09-29 13:39:26 +0500

    task2

--- scripts/script2.js
+++ scripts/script2.js
@@ -1,8 +1,7 @@
-console.log("Задачка 1 \n для y=a*cosx c шагом п = п/8 при -п < x < п");
-const a = 5;
-let x = -Math.PI;
+console.log("Задачка 2");
+let pr = 0;
 
-while (x<Math.PI) {
-    console.log("при x = " + x + "\ny = " + a*Math.cos(x));
-    x += Math.PI/8;
+for (let i = 3; i <=49; i++) {
+    pr+=i;
 }
+console.log("Произведение = " + pr);
